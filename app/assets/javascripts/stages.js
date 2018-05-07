8d1e891a18ea96ad0825ff006ddb9ca1a01ce41c
AutumnIDavis <foreverautumn16@outlook.com>
2018-05-07 17:33:00 -0400

    Sounds timed

--- app/assets/javascripts/stages.js
+++ app/assets/javascripts/stages.js
@@ -17,31 +17,31 @@ function rain() {
 // Allegra voice over
 function Allegra() {
   var voice1 = document.getElementById("Allegra")
-  setInterval(function(){ voice1.play(); }, 5000);
+  setTimeout(function(){ voice1.play(); }, 34000);
 };
 
 // Frida voice over
 function Frida() {
   var voice2 = document.getElementById("Frida")
-  setInterval(function(){ voice2.play(); }, 7000);
+  setTimeout(function(){ voice2.play(); }, 36000);
 };
 
 // Sharif voice over
 function Sharif() {
   var voice3 = document.getElementById("Sharif")
-  setInterval(function(){ voice3.play(); }, 9000);
+  setTimeout(function(){ voice3.play(); }, 42000);
 };
 
 // Clayton voice over
 function C_Because() {
   var voice4 = document.getElementById("C_Because")
-  setInterval(function(){ voice4.play(); }, 2000);
+  setTimeout(function(){ voice4.play(); }, 2000);
 };
 
 // Clayton voice over2
 function C_Board() {
   var voice5 = document.getElementById("C_Board")
-  setInterval(function(){ voice5.play(); }, 3500);
+  setTimeout(function(){ voice5.play(); }, 3500);
 };
 
 
